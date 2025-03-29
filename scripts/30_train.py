a67a80cd64a367ea2d18968c3c85ce3d21366a48
Nehiz <88313543+Nehiz@users.noreply.github.com>
2025-03-29 19:34:37 +0100

    latest update

--- scripts/30_train.py
+++ scripts/30_train.py
@@ -33,8 +33,8 @@ do_hyperparameter_optimization = False  # set to False to use the default hyperp
 n_hpo_iters = 20  # number of hyperparameter optimization iterations
 n_jobs = -1  # number of jobs to run in parallel (set to 1 if your laptop gets too hot)
 
-in_path = "training file goes here"
-out_dir = "output directory goes here"
+in_path = r"C:\Users\nehik\OneDrive\Documents\recur\csv\recur_scan_train - train.csv"
+out_dir = r"C:\Users\nehik\OneDrive\Documents\recur\output"
 
 # %%
 # parse script arguments from command line
