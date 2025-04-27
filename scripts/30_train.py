94a6e6ce712ad246f1c74e9f8d9bd4fd965b33ad
Felix Wilson Gbedemah <afrogbede09@gmail.com>
2025-04-27 00:17:47 +0000

    updates made to the 30_train.py

--- scripts/30_train.py
+++ scripts/30_train.py
@@ -27,26 +27,28 @@ from sklearn.model_selection import GridSearchCV, GroupKFold, RandomizedSearchCV
 from tqdm import tqdm
 
 from recur_scan.features import get_features
-
-from recur_scan.features_felix import get_new_features
-from recur_scan.transactions import group_transactions, read_labeled_transactions, write_transactions
+from recur_scan.features_original import get_new_features
+from recur_scan.transactions import (
+    group_transactions,
+    read_labeled_transactions,
+    write_labeled_transactions,
+    write_transactions,
+)
 
 # %%
 # configure the script
 
 use_precomputed_features = True
 model_type = "xgb"  # "rf" or "xgb"
-n_cv_folds = 3  # number of cross-validation folds, could be 5
+n_cv_folds = 5  # number of cross-validation folds, could be 5
 do_hyperparameter_optimization = False  # set to False to use the default hyperparameters
 search_type = "random"  # "grid" or "random"
 n_hpo_iters = 200  # number of hyperparameter optimization iterations
 n_jobs = -1  # number of jobs to run in parallel (set to 1 if your laptop gets too hot)
 
-
 in_path = "../../data/train.csv"
 precomputed_features_path = "../../data/train_features.csv"
 out_dir = "../../data/training_out"
-
 
 # %%
 # parse script arguments from command line
